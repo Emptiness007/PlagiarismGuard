d74a5ce3161e089fe31be5585c4fc8cab9686080
Kristina.Gordeeva <kgordeeva957@gmail.com>
2025-03-25 20:13:35 +0500

    Написана библиотека стилей и реализован интерфейс главного окна

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,37 @@ namespace PlagiarismGuard
         public MainWindow()
         {
             InitializeComponent();
+
+            // Создаем коллекцию данных
+            var sources = new ObservableCollection<Source>
+            {
+                new Source
+                {
+                    SourceNo = 1,
+                    SourceName = "Source A",
+                    Excerpt = "This is an example excerpt from the source.",
+                    Similarity = 20, // Процент сходства
+                }
+            };
+
+            // Привязываем данные к DataGrid
+            SourceDataGrid.ItemsSource = sources;
         }
+
+        // Обработчик кнопки "View Report"
+        private void ViewReport_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("View Report clicked!");
+        }
+    }
+
+    // Класс для представления данных
+    public class Source
+    {
+        public int SourceNo { get; set; }
+        public string SourceName { get; set; }
+        public string Excerpt { get; set; }
+        public double Similarity { get; set; }
+        public string Url { get; set; }
     }
 }
