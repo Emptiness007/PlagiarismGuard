e7c20cbfe45b267e6d1390221bb1751e7e2c3858
emptiness <alenafilimonova98@gmail.com>
2025-04-07 16:04:12 +0500

    Добавлен контекст бд

--- Data/PlagiarismContext.cs
+++ Data/PlagiarismContext.cs
@@ -21,5 +21,74 @@ namespace PlagiarismGuard.Data
         {
             optionsBuilder.UseMySql("Server=127.0.0.1;Database=Plagiarism;Port=3307;user=root;pwd=;", new MySqlServerVersion(new Version(8, 0, 11)));
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка enum для ролей пользователей
+            modelBuilder.Entity<User>()
+                .Property(u => u.Role)
+                .HasConversion<string>()
+                .HasColumnType("varchar(5)")
+                .HasDefaultValue("user")
+                .IsRequired();
+
+            // Настройка enum для форматов документов
+            modelBuilder.Entity<Document>()
+                .Property(d => d.Format)
+                .HasConversion<string>()
+                .HasColumnType("varchar(4)")
+                .IsRequired();
+
+            // Уникальные ограничения
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            // Связи
+            modelBuilder.Entity<Document>()
+                .HasOne(d => d.User)
+                .WithMany()
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<DocumentText>()
+                .HasOne(dt => dt.Document)
+                .WithMany()
+                .HasForeignKey(dt => dt.DocumentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Check>()
+                .HasOne(c => c.Document)
+                .WithMany()
+                .HasForeignKey(c => c.DocumentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Check>()
+                .HasOne(c => c.User)
+                .WithMany()
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CheckResult>()
+                .HasOne(cr => cr.Check)
+                .WithMany()
+                .HasForeignKey(cr => cr.CheckId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CheckResult>()
+                .HasOne(cr => cr.SourceDocument)
+                .WithMany()
+                .HasForeignKey(cr => cr.SourceDocumentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Report>()
+                .HasOne(r => r.Check)
+                .WithMany()
+                .HasForeignKey(r => r.CheckId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
